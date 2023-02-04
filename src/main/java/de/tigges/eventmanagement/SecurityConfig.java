c863c4bb758702098c9c86770398540e66f32cf3
johannestigges <johannes.tigges@freenet.de>
2023-02-04 19:33:32 +0100

    add dirigent

--- src/main/java/de/tigges/eventmanagement/SecurityConfig.java
+++ src/main/java/de/tigges/eventmanagement/SecurityConfig.java
@@ -1,16 +1,22 @@
 package de.tigges.eventmanagement;
 
 import de.tigges.eventmanagement.UsersData.UserData;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -25,9 +31,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .formLogin().and()
-                .authorizeHttpRequests().and()
-                .authorizeHttpRequests().anyRequest().authenticated().and()
+                .formLogin()
+                .loginPage("/#/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/")
+                .failureHandler(new AppAuthenticationFailureHandler())
+                .and()
+                .authorizeHttpRequests(requests ->
+                        requests.requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/rest/**")).authenticated()
+                                .anyRequest().permitAll())
                 .csrf().disable()
                 .build();
     }
@@ -35,16 +48,24 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService users() {
         return new InMemoryUserDetailsManager(
-            Arrays.stream(usersData.getUsers())
-                .map(SecurityConfig::map)
-                .collect(Collectors.toSet()));
+                Arrays.stream(usersData.getUsers())
+                        .map(SecurityConfig::map)
+                        .collect(Collectors.toSet()));
     }
 
     private static UserDetails map(UserData user) {
         return User.builder()
-            .username(user.getUsername())
-            .password(user.getPassword())
-            .roles(user.getRoles())
-            .build();
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRoles())
+                .build();
+    }
+
+    private static class AppAuthenticationFailureHandler implements AuthenticationFailureHandler {
+        @Override
+        public void onAuthenticationFailure(
+                HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        }
     }
 }
